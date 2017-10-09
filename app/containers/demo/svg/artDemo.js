0a9e05630c33e0961951006d1d38000567715232
Aevit <Aevitx@gmail.com>
2017-10-09 18:13:28 +0800

    draw circle

--- app/containers/demo/svg/artDemo.js
+++ app/containers/demo/svg/artDemo.js
@@ -3,7 +3,7 @@
  * @Desc:
  * @Date: 2017-09-28 16:14:25
  * @Last Modified by: Aevit
- * @Last Modified time: 2017-09-30 15:17:34
+ * @Last Modified time: 2017-10-09 18:12:50
  */
 'use strict'
 import {
@@ -18,20 +18,20 @@ const { Surface, Shape, Group, Path } = ART
 class Circle extends React.Component {
   circlePath (cx, cy, r, startDegree, endDegree) {
     let p = Path()
-    p.path.push(0, cx + r, cy)
-    p.path.push(4, cx, cy, r, startDegree * Math.PI / 180, endDegree * Math.PI / 180, 1)
+    p.path.push(0, cx, cy - r)
+    p.path.push(4, cx, cy, r, startDegree * Math.PI / 180, endDegree * Math.PI / 180, 1) // 第一个 4 是画弧（可查看 ./node_modules/react-native/Libraries/ART/RCTConvert+ART.m 里 “ + (CGPathRef)CGPath:(id)json ” 方法）
     return p
   }
 
   render () {
-    const size = 100
-    const storkeWidth = 2
-    const circlePath = this.circlePath(size / 2, size / 2, size / 2 - storkeWidth / 2, 0, (360 * 0.9999) * this.props.fill / 100)
+    const size = 200
+    const strokeWidth = 2
+    const circlePath = this.circlePath(size / 2, size / 2, size / 2 - strokeWidth / 2, 270, 270 + 360 * this.props.fill / 100)
     return (
       <View>
-        <Surface width={this.props.width || 100} height={this.props.height || 100}>
+        <Surface width={this.props.width || 300} height={this.props.height || 300}>
           <Group>
-            <Shape d={circlePath} stroke='#000000' strokeWidth={3} />
+            <Shape d={circlePath} stroke='#000000' strokeWidth={strokeWidth} />
           </Group>
         </Surface>
       </View>
@@ -49,7 +49,7 @@ export default class svg extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      circleProgress: new Animated.Value(50)
+      circleProgress: new Animated.Value(0)
     }
   }
 
